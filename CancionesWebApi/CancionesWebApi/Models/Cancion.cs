010170055aae0d190feaf50dcc7879cf9504c7a8
Camila Crapuzzi Gutierrez <a2020117908@estudiantes.upsa.edu.bo>
2021-10-13 13:36:12 -0600

    Modelo Cancion craedo

---
+++ CancionesWebApi/CancionesWebApi/Models/Cancion.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CancionesWebApi.Models
+{
+    public class Cancion
+    {
+        [Key]
+        public string Nombre { get; set; }
+        [Required(AllowEmptyStrings =false,ErrorMessage ="{0} es requerido")]
+        public string Autor { get; set; }
+        [Required(AllowEmptyStrings =false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200,MinimumLength =10, ErrorMessage = "La longitud de{0} debe estar entre {2} y {1}")]
+        public string Letra { get; set; }
+        [Url]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de{0} debe estar entre {2} y {1}")]
+        public string Enlace { get; set; }
+
+    }
+}
+
